13db07cb1c24c4c00c7f1c6208c77a4f86d10aa5
rm13082016 <rm13082016@outlook.com>
2019-11-05 19:36:46 +0000

    Added Files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+
+
+app.listen(4205, () => console.log('Server listening on port 4205!'));
